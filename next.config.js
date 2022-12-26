43dee16861afffcc5e1c6ce149750ff93fccc247
singlexyz <singlexyz@gmail.com>
2022-12-27 00:28:44 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  basePath: "/next"
+  reactStrictMode: true
+  // basePath: "/next"
 }
 
 module.exports = nextConfig
